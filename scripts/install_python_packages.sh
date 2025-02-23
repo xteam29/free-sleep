a730f397177a3b5c04e0a94bd9c812a011a6b3c8
Noah Meier <noahmeier82@proton.me>
2025-02-23 11:36:38 -0600

    Fix pyexpat for python 3.9

--- scripts/install_python_packages.sh
+++ scripts/install_python_packages.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-cp /home/dac/free-sleep/scripts/python/plistlib.py /usr/lib64/python3.10/
 
-# TODO: need the version for python 3.9
-cp /home/dac/free-sleep/scripts/python/pyexpat.cpython-310-aarch64-linux-gnu.so /usr/lib64/python3.10/
+
+PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+PYTHON_VERSION_2=$(python3 -c 'import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")')
+echo "Python version: $PYTHON_VERSION_2"
+
+cp /home/dac/free-sleep/scripts/python/plistlib.py /usr/lib64/python$PYTHON_VERSION/
+cp /home/dac/free-sleep/scripts/python/pyexpat.cpython-$PYTHON_VERSION_2-aarch64-linux-gnu.so /usr/lib64/python$PYTHON_VERSION/
 python3 -m venv /home/dac/venv
 source /home/dac/venv/bin/activate
 /home/dac/venv/bin/python -m pip install numpy scipy pandas cbor2 watchdog
+
