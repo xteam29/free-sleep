2591cfa23e2be103ceaef22d8898790b81dd7f85
Noah Meier <noahmeier82@proton.me>
2025-02-23 10:44:42 -0600

    Fix enable biometrics script

--- scripts/enable_biometrics.sh
+++ scripts/enable_biometrics.sh
@@ -2,6 +2,6 @@
 
 sh /home/dac/free-sleep/scripts/unblock_internet_access.sh
 sh /home/dac/free-sleep/scripts/setup_python.sh
-sh /home/dac/free-sleep/scripts/install_python.sh
+sh /home/dac/free-sleep/scripts/install_python_packages.sh
 sh /home/dac/free-sleep/scripts/setup_streamer_service.sh
 sh /home/dac/free-sleep/scripts/block_internet_access.sh
