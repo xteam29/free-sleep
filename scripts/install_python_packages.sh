6c9b740d2a6686cc409f5b9cab39d7b6313266b4
Noah Meier <noahmeier82@proton.me>
2025-02-23 16:39:30 -0600

    Fix folder path issue for python 3.9

--- scripts/install_python_packages.sh
+++ scripts/install_python_packages.sh
@@ -5,7 +5,7 @@ PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}{sys.ve
 
 if [ "$PYTHON_VERSION" = "39" ]; then
     # Pod 3 with SD card has python version 3.9 & is missing venv
-    cp -r /home/dac/free-sleep/scripts/python/$PYTHON_VERSION_DOT/venv /usr/lib64/python$PYTHON_VERSION_DOT/
+    cp -r /home/dac/free-sleep/scripts/python/$PYTHON_VERSION/venv /usr/lib64/python$PYTHON_VERSION_DOT/
     cp /home/dac/free-sleep/scripts/python/plistlib.py /usr/lib/python$PYTHON_VERSION_DOT/
     cp /home/dac/free-sleep/scripts/python/pyexpat.cpython-$PYTHON_VERSION-aarch64-linux-gnu.so /usr/lib/python$PYTHON_VERSION_DOT/
     echo "Copied files for Pod 3 with SD card successfully."
