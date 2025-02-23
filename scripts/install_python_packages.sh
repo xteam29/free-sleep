0cceaf782c9f0d9ea2b656a48a4e4eacb1c432cb
Noah Meier <noahmeier82@proton.me>
2025-02-23 11:55:45 -0600

    Add venv for Pod without sd card

--- scripts/install_python_packages.sh
+++ scripts/install_python_packages.sh
@@ -5,6 +5,11 @@ PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.v
 PYTHON_VERSION_2=$(python3 -c 'import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")')
 echo "Python version: $PYTHON_VERSION_2"
 
+if [ "$PYTHON_VERSION_2" = "39" ]; then
+    cp -r /home/dac/free-sleep/scripts/python/$PYTHON_VERSION/venv /usr/lib64/python$PYTHON_VERSION/
+    echo "Copied venv successfully."
+fi
+
 cp /home/dac/free-sleep/scripts/python/plistlib.py /usr/lib64/python$PYTHON_VERSION/
 cp /home/dac/free-sleep/scripts/python/pyexpat.cpython-$PYTHON_VERSION_2-aarch64-linux-gnu.so /usr/lib64/python$PYTHON_VERSION/
 python3 -m venv /home/dac/venv
